a553ffe5c8f075612ed0cdf39d1584c99257a6df
Christian A <christianajusd@gmail.com>
2017-03-23 07:18:15 +0000

    Finsihed main part of experiment code

--- ping.py
+++ ping.py
@@ -3,25 +3,24 @@ import subprocess
 import re
 
 FILENAME = raw_input("What antenna are you testing? ")
-DEGREES = raw_input("How many degrees will you test?")
-ADDRESS = raw_input("fc:58:fa:13:2f:b8")
+#DEGREES = raw_input("How many degrees will you test?")
+#ADDRESS = raw_input("fc:58:fa:13:2f:b8")
 
 file = []
 for x in range(0,5):
-	try:
+	raw_input("please turn " +str(x*60)+"degrees")
+	row = []
+	row.append(x*60)
+	for y in range (0,3):
 		ping = subprocess.check_output(['hcitool','rssi', 'fc:58:fa:13:2f:b8'])
 		print("Pinging...")
-	except Exception:
-		ping = "time err"
-		print("could not connect")
-	values = re.findall(r'value: (\d+\.\d+)', ping)
-	file.append(x*30)
-	file.append(values)
-	raw_input("please turn "+ str(30*x)+ " and press enter")
+		values = ping.split(': ')[1].rstrip()
+		row.append(values)
+	file.append(row)
 print(file)
 
 
 with open(FILENAME, 'wb') as outcsv:
 	writer = csv.writer(outcsv)
 	writer.writerows(file)
-subprocess.call(['chown','christian',FILENAME])
+#subprocess.call(['chown','christian',FILENAME])
